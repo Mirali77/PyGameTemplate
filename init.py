e8bfd3b84b764c967ce83da68d77452892a593f3
Mirali77 <79355083+Mirali77@users.noreply.github.com>
2022-06-28 10:40:57 +0300

    New template

---
+++ init.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+
+WIDTH = 360  # ширина игрового окна
+HEIGHT = 480  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+# Цвета (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+images = []
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
